547deb198ab0ac1770fe458144438901fc88b5a4
Cindy Williams <cindy.williams@volstate.edu>
2018-05-03 15:55:03 -0500

    minimizing

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
   
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   
-  <script src="js/maps.js" type="text/javascript"></script>
+  <script src="js/maps.min.js" type="text/javascript"></script>
 
 
 </body>
