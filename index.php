e4393bddd6004173419bb32f6a9567992020ed0d
Cindy Williams <cindy.williams@volstate.edu>
2018-05-03 15:57:24 -0500

    semantics

--- index.php
+++ index.php
@@ -4,27 +4,31 @@
   
   <div class="row no-gutters">
     
+    <!-- sidebar -->
     <aside class="col-md-3" id="side_bar">
       
-      <main class="logo">
+      <div class="logo">
         <a href="https://www.volstate.edu" title="Volunteer State Community College" rel="home" class="site-branding__logo">
           <img src="img/volstate_logo.png" alt="Volunteer State Community College">
         </a>
-      </main>
+      </div>
       
       <input type="radio" name="campus" value="main" id="main" checked="checked" class="radio_btns">
       <ul id="main_list"></ul>
     
     </aside>
 
-    <div class="col-md-9">
+    <!-- map -->
+    <main class="col-md-9">
       <div id="map_canvas"></div>
+    </main>
 
   </div>  <!-- row -->
 
-  <div class="row no-gutters footer">
+  <!-- footer -->
+  <footer class="row no-gutters footer">
     <div class="col-md-12">Copyright &copy; <?php echo date("Y"); ?> Volunteer State Community College</div>
-  </div>
+  </footer>
 
 </div>  <!-- container -->
 
