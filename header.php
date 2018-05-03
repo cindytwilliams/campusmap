6f462ee33dba7c25c44e6730abab1ef9cad29cf2
Cindy Williams <cindy.williams@volstate.edu>
2018-05-03 13:58:45 -0500

    responsive

--- header.php
+++ header.php
@@ -3,13 +3,17 @@
 <html>
 <head>
 
-<title>Campus Map</title>
+  <title>Volunteer State Community College - Interactive Campus Map</title>
+
+  <link rel="shortcut icon" href="favicon.ico" />
 
   <link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAatP3LXSEWmW7SgAGLeEy2VLi69yzbC5g"></script>
   <script src="js/maps.min.js" type="text/javascript"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
 </head>
 
--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php include 'header.php' ?>
   
-  <div id="content">
-  
-    <main id="map_canvas"></main>
+  <div class="row">
     
-    <nav id="side_bar">
+    <aside class="col-md-4" id="side_bar">
       
       <div class="logo">
         <a href="https://www.volstate.edu" title="Volunteer State Community College" rel="home" class="site-branding__logo">
           <img src="img/volstate_logo.png" alt="Volunteer State Community College">
         </a>
       </div>
-
+      
       <input type="radio" name="campus" value="main" id="main" checked="checked" class="radio_btns">
       <ul id="main_list"></ul>
-    </nav>  <!-- side_bar -->
+    
+    </aside>
 
+    <main class="col-md-8" id="map_canvas"></main>
 
-  </div>  <!-- content -->
+  </div>  <!-- row -->
 
 <?php include 'footer.php' ?>
