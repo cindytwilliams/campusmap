5ba23ba1d688752109774695a9eacca38c40b874
Cindy Williams <cindy.williams@volstate.edu>
2018-05-03 12:40:46 -0500

    split out header and footer

---
+++ header.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Campus Map</title>
+
+  <link rel="stylesheet" type="text/css" href="css/style.min.css">
+  
+  <script src="https://maps.googleapis.com/maps/api/js" type="text/javascript"></script>
+  <script src="js/maps.min.js" type="text/javascript"></script>
+
+</head>
+
+<body>  
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include 'header.php' ?>
+  
+  <div id="content">
+  
+    <div id="map_canvas"></div>
+    
+    <div id="side_bar">
+      <input type="radio" name="campus" value="main" id="main" checked="checked" class="radio_btns">
+      <ul id="main_list"></ul>
+    </div>  <!-- side_bar -->
+
+
+  </div>  <!-- content -->
+
+<?php include 'footer.php' ?>
