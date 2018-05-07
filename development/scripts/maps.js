6b88e7a755c77650c597bf6a05abf7b261634e35
Cindy Williams <cindy.williams@volstate.edu>
2018-05-07 13:29:18 -0500

    open popup when sidebar link is focused

--- development/scripts/maps.js
+++ development/scripts/maps.js
@@ -171,6 +171,15 @@ for ( i = 0; i < bldgsList.length; i++) {
 ul = document.getElementById( "main_list" );
 ul.focus();
 
+// open popup when you click or focus on a sidebar link
+$("li").click(function(){
+  markerFunction($(this)[0].id);
+});
+$("li").focus(function(){
+  markerFunction($(this)[0].id);
+});
+
+// bind popup to sidebar link
 function markerFunction(id){
   for (var i in markers){
       var markerID = markers[i].options.title;
@@ -179,148 +188,3 @@ function markerFunction(id){
       };
   }
 }
-
-$("li").click(function(){
-    markerFunction($(this)[0].id);
-});
-
-
-
-/* GOOGLE MAPS
-
-// Create a infoWindow for all markers
-var infoWnd = new google.maps.InfoWindow({ maxWidth: 375 });
-
-// To control markers' visibility by radio buttons.
-var markerController = new google.maps.MVCObject();
-
-// Initialization
-function initialize() {
-  
-  // Create a map
-  var mapDiv = document.getElementById("map_canvas");
-  var myOptions = {
-    zoom: 8,
-    mapTypeId: google.maps.MapTypeId.HYBRID
-  };
-  mapCanvas = new google.maps.Map(mapDiv,myOptions);
-  
-  // selectChanged function will be involved when a radio button is clicked.
-  var i, choice = [ 
-    document.getElementById("main")
-  ];
-  for (i = 0; i < choice.length; i++) {
-    google.maps.event.addDomListener(choice[i], "click", selectChanged);
-  }
-  
-  // Putting markers onto the map
-  var bounds = new google.maps.LatLngBounds();
-  var campus, latlng;
-  
-  // loop through buildings
-  for ( i = 0; i < bldgsList.length; i++) {
-    campus = bldgsList[i];
-    latlng = new google.maps.LatLng(campus.latlng[0], campus.latlng[1]);
-    bounds.extend(latlng);
-    var marker = createMarker({
-      map : mapCanvas,
-      position : latlng,
-      others : campus
-    });
-
-    // Create side-bar buttons
-    createMarkerButton(marker);
-  }
-  // Fits the viewport to the bounds.
-  mapCanvas.fitBounds(bounds);
-  
-  // Sets the initial selected value
-  markerController.set("select", "main");
-  google.maps.event.trigger(choice[0], "click");
-}
-
-// This function will be involved when a radio button is clicked.
-// select_changed events will be fired for every markers when the markerController's property is updated.
-function selectChanged() {
-  var selectedVal = this.value;
-  markerController.set("select", selectedVal);
-  
-  // Changes visibilities for each <ul>.
-  var i, ul, listNames = ["main"];
-  for (i = 0; i < listNames.length; i++) {
-    ul = document.getElementById(listNames[i] + "_list");
-    if (listNames[i] === selectedVal) {
-      ul.style.display = "block";
-    } else {
-      ul.style.display = "none";
-    }
-  }
-}
-
-// Create a marker.
-function createMarker(params) {
-  var marker = new google.maps.Marker(params);
-
-  // When the marker is clicked or hovered, the infoWindow is displayed.
-  google.maps.event.addListener(marker, "click", function() {
-    popup(params, marker);
-  });
-  google.maps.event.addListener(marker, "mouseover", function() {
-    popup(params, marker);
-  });
-
-  // Binds the select properties between marker and markerController.
-  // If markerController.select is changed, then marker.select would be changed, and
-  // a select_changed event is fired.
-  marker.bindTo("select", markerController, "select");
-  google.maps.event.addListener(marker, "select_changed", changeMarkerVisibility);
-  return marker;
-}
-
-// Content of infoWindow
-function popup(params, marker) {
-  infoWnd.setContent("<div class='popup'><img src='" + params.others.photo + "'><p><strong>" + params.others.name + "</strong></p><p>" + params.others.desc + "</p></div>");
-  infoWnd.open(params.map, marker);
-}
-
-// If markerController.select is changed, 
-// then check the each value of params.others,
-// and change the visibility of the marker.
-function changeMarkerVisibility() {
-  // "this" means "marker".
-  var marker = this;
-  var others = marker.get("others");
-  
-  // The selected value of radio buttons
-  var selectedVal = marker.get("select");
-  
-  // Determine the marker's visibility.
-  marker.setVisible( others[selectedVal] ? true : false );
-}
-
-// Create buttons each <ul>s.
-function createMarkerButton(marker) {
-  var others = marker.get("others"),
-      i, name, ul, li,
-      listNames = ["main"];
-  
-  // Only creates a button when the propery is 1
-  for (i = 0; i < listNames.length; i++) {
-    name = listNames[i];
-    if (others[ name ]) {
-      ul = document.getElementById( name + "_list" );
-      li = document.createElement("li");
-      li.innerHTML = others.name;
-      ul.appendChild(li);
-      
-      // If the button is clicked, then the marker would be clicked.
-      google.maps.event.addDomListener(li, "click", function() {
-        google.maps.event.trigger(marker, "click");
-      });
-    }
-  }
-  
-}
-
-google.maps.event.addDomListener(window, "load", initialize);
-*/
